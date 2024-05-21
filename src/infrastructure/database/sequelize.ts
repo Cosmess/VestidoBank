285de7552759dd7391e7f8b5ab243dec429a5f0a
cosmesousa <36869385+Cosmess@users.noreply.github.com>
2024-05-21 12:15:49 -0300

    add env

--- src/infrastructure/database/sequelize.ts
+++ src/infrastructure/database/sequelize.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../../domain/entities/User';
 import { Transaction } from '../../domain/entities/Transaction';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  database: 'vestidobank',
-  dialect: 'mysql',
-  username: 'root',
-  password: '', 
-  host: 'localhost',
+  database: process.env.DB_DATABASE,
+  dialect: process.env.DB_DIALECT as 'mysql' | undefined,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
   models: [User, Transaction],
 });
